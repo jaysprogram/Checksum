b4d681723e92e43f9d7d5097069b4a35ff0d722b
jaysprogram <107363449+jaysprogram@users.noreply.github.com>
2024-10-29 23:41:41 -0400

    done?

--- pa02.py
+++ pa02.py
@@ -11,7 +11,7 @@ def calculateChecksum(checksumSize,fileName):
         sys.exit(1)
             
     chars = len(text)
-    paddingNeeded = len(chars) % (checksumSize // 8)
+    paddingNeeded = chars % (checksumSize // 8)
     if paddingNeeded != 0:
         padSize = (checksumSize // 8) - paddingNeeded
         text += b'X' * padSize ### padding with binary X to make it divisble by checksumsize
@@ -21,27 +21,29 @@ def calculateChecksum(checksumSize,fileName):
     
     ## add to sum till the end of text
     for i in range(0, len(text), chunkSize):
-        chunk = int.from_bytes(text[i:i + chunkSize])
+        chunk = int.from_bytes(text[i:i + chunkSize], "big")
         checkSum = (checkSum + chunk) & ((1 << checksumSize) - 1)
         
-    
-     
+    ## take the two's complement
+    checkSum = (~checkSum + 1) & (( 1 << checksumSize) - 1)
 
-    return "67677", chars 
+    return checkSum, chars 
     
         
 def main():
     #arguments from command lines
     fileName = sys.argv[1]
     checkSumSize = int(sys.argv[2]) ## in bits 8 / 16 / 32
-    if checkSumSize not in [8,16.32]:
+    if checkSumSize not in [8,16,32]:
         print("Valid checksum sizes are 8, 16, or 32\n",file = sys.stderr)    
         sys.exit(1) 
 
     #calculate checksum
     result, chars = calculateChecksum(checkSumSize,fileName)
+    result = hex(result)
+    str = result[2:]
     
-    print(f"{checkSumSize:2} bit checksum is {result:8} for all {chars:4}\n")
+    print(f"{checkSumSize:2} bit checksum is {str:8} for all {chars:4} chars\n")
 
 if __name__ == "__main__":
     main()
